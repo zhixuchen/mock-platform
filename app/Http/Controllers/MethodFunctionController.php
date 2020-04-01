a17e6534333a8e11905936133f686707847cf525
sentiger <shi_wenhu@qq.com>
2020-04-01 11:49:01 +0800

    万能路由

--- app/Http/Controllers/MethodFunctionController.php
+++ app/Http/Controllers/MethodFunctionController.php
@@ -10,11 +10,12 @@ use App\Models\MockProject;
 
 class MethodFunctionController extends Controller
 {
-    public static function getmethod_uri($uri){
+    public static function getmethod_uri($uri)
+    {
         $methodRes = MockProjectMethod::get();
-        foreach  (json_decode($methodRes) as $value){
-            $method_uri=$value->uri;
-            if(strpos($uri,$method_uri) or strpos($uri,$method_uri)===0) {
+        foreach (json_decode($methodRes) as $value) {
+            $method_uri = $value->uri;
+            if (strpos($uri, $method_uri) or strpos($uri, $method_uri) === 0) {
                 $method_uri = $method_uri;
                 return $method_uri;
             }
@@ -36,9 +37,8 @@ class MethodFunctionController extends Controller
 //        mysqli_close($conn);
     }
 
-    public static function getmethod_id($data,$uri)
+    public static function getmethod_id($data, $uri)
     {
-        $data=json_decode(json_encode($data));
         $methodRes = MockProject::select(
             'mock_project.id',
             'mock_project_method.project_id',
@@ -50,76 +50,111 @@ class MethodFunctionController extends Controller
             ->where('mock_project_method.uri', $uri)
             ->get();
 
-        foreach (json_decode($methodRes) as $methodRe) {
+        foreach ($methodRes as $methodRe) {
             $rule = $methodRe->rule;
-
-//            $value=$data->busiCode;
-            echo $rule;
             eval($rule);
-            echo $value;
-//
         }
     }
 
 
-    public static function getvin(){
+    public static function getvin()
+    {
         $chars = array(
-            "A", "B", "C", "D", "E", "F", "G",
-            "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-            "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2",
-            "3", "4", "5", "6", "7", "8", "9"
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"
         );
 
 
         $charsLen = count($chars) - 1;
         shuffle($chars);                            //打乱数组顺序
         $str = '';
-        for($i=0; $i<17; $i++){
+        for ($i = 0; $i < 17; $i++) {
             $str .= $chars[mt_rand(0, $charsLen)];    //随机取出一位
         }
         return $str;
     }
 
-    public static function gettongdun_id(){
+    public static function gettongdun_id()
+    {
         $time = time();
         $n = rand(0, 9);
         $id = "WF20181207180" . $time . $n;
         return $id;
     }
 
-    public static function getcreatbusinessno(){
-        $Businessno="CL".time() . "000";;
+    public static function getcreatbusinessno()
+    {
+        $Businessno = "CL" . time() . "000";;
         return $Businessno;
     }
 
-    public static function getbusinessno($data){
-        $Businessno=json_decode($data)->Businessno;
+    public static function getbusinessno($data)
+    {
+        $Businessno = json_decode($data)->Businessno;
         // echo "12312";
         return $Businessno;
     }
 
-    public static function getestageOrderNo(){
-        $estageOrderNo='test'.md5(time());
+    public static function getestageOrderNo()
+    {
+        $estageOrderNo = 'test' . md5(time());
         return $estageOrderNo;
     }
 
-    public static function getorderNo($data){
-        $orderNo=json_decode($data)->pub->orderNo;
+    public static function getorderNo($data)
+    {
+        $orderNo = json_decode($data)->pub->orderNo;
         return $orderNo;
 
 
     }
 
-    public static function getSign($data,$businessno,$status){
-        $Sign=md5("message=测试回调"."ordernum=".$businessno."status=".$status. "statusdes=订单状态tokenid=119userid=1122408895b2e-1f70-4539-97a2-3454f8986b08");
-        $Sign=strtoupper($Sign);
+    public static function getSign($data, $businessno, $status)
+    {
+        $Sign = md5("message=测试回调" . "ordernum=" . $businessno . "status=" . $status . "statusdes=订单状态tokenid=119userid=1122408895b2e-1f70-4539-97a2-3454f8986b08");
+        $Sign = strtoupper($Sign);
         return $Sign;
 
     }
 
-    public static function get_url($platform,$env){
+    public static function get_url($platform, $env)
+    {
 
-        $url="http://".$env."-".$platform."-docker-".$env.".beta.saasyc.com";
+        $url = "http://" . $env . "-" . $platform . "-docker-" . $env . ".beta.saasyc.com";
         return $url;
 
     }
